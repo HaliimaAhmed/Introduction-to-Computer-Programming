81a18fe3c2f6591a645a4b964738eb522d3d8a12
Haliima Ahmed <43786387+HaliimaAhmed@users.noreply.github.com>
2020-11-18 19:43:36 -0500

    Add files via upload

---
+++ CalculateGcf/src/CalculateGcf.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+/*********************************************************************************************
+ * 
+ * Student Name:Halima Ahmed
+ * Course:Introduction to computer programming
+ * Semester: FALL 2018
+ * This class calculates the greatest common factor of two numbers.  
+ */
+public class CalculateGcf {
+
+	private int value1, value2, gcf = 0;
+
+	Scanner scan = new Scanner(System.in);
+
+	public void getValues() {
+		System.out.println("Enter value1: ");
+		value1 = scan.nextInt();
+		System.out.println("Enter value2: ");
+		value2 = scan.nextInt();
+	}
+
+	public void calculateGcf() {
+
+		{
+			if (value1 < 0) {
+				System.out.println("Enter value1 that is greater than 0: ");
+				value1 = scan.nextInt();
+
+			}
+			if (value2 < 0) {
+				System.out.println("Enter value2 that is greater than 0:");
+				value2 = scan.nextInt();
+			}
+		}
+
+		int a = value1;
+		int b = value2;
+
+		while (a != 0) {
+			gcf = a;
+			a = b % a;
+			b = gcf;
+		}
+	}
+
+	public void display() {
+		System.out.println("The greatest common factor of  " + (value1) + " and " + (value2) + " is " + (gcf));
+
+	}
+}
---
+++ CalculateGcf/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+/*********************************************************************************************
+ * 
+ * Student Name:Halima Ahmed
+ * Course:Introduction to computer programming
+ * Semester: FALL 2018
+ * This class is the main class of CalculateGcf.
+ */
+	public static void main(String[] args) {
+		System.out.println(" Program developed by Halima Ahmed  ");
+		CalculateGcf value = new CalculateGcf();
+		value.getValues();
+		value.calculateGcf();
+		value.display();
+	}
+}
